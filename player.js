c567354b1d9c8583ca751c6a3088461637928912
Henry Ripple <hripple46@gmail.com>
2023-01-04 11:46:13 -0600

    added a Computer that plays moves; Computer will not play moves on squares that have already been hit

--- player.js
+++ player.js
@@ -1,3 +1,4 @@
+import { parse } from "@babel/core";
 import { receiveAttack } from "./gameboard";
 
 function Player(name) {
@@ -6,5 +7,24 @@ function Player(name) {
   }
   return { name, attack };
 }
+function Computer() {
+  function attack(gameBoard) {
+    let gridPoint = Math.floor(Math.random() * gameBoard.grid.length);
+    if (
+      gameBoard.grid[gridPoint] == null ||
+      gameBoard.grid[gridPoint].isSunk == false
+    ) {
+      let yString = gridPoint.toString().charAt(0);
+      let y = parseInt(yString);
+      let xString = gridPoint.toString().charAt(1);
+      let xInt = parseInt(xString);
+      let x = String.fromCharCode(97 + xInt);
+      return gameBoard.receiveAttack(x, y);
+    } else {
+      return Computer.attack(gameBoard);
+    }
+  }
+  return { attack };
+}
 
-export { Player };
+export { Player, Computer };
--- player.test.js
+++ player.test.js
@@ -1,4 +1,4 @@
-import { Player } from "./player";
+import { Player, Computer } from "./player";
 import { Ship, hit, isSunk } from "./ship";
 import { Gameboard } from "./gameboard";
 let Henry = Player("Henry");
@@ -6,10 +6,16 @@ let garyBoard = Gameboard();
 let Gary = Ship(2, 0, false);
 garyBoard.placeShip(Gary, 1, "horizontal");
 
+let comp = Computer();
+
 test("Player attacks garyBoard", () => {
   expect(Henry.attack(garyBoard, "b", 0)).toEqual({
     length: 2,
     hit: 1,
     sunkStatus: false,
   });
+});
+
+test("Computer attacks", () => {
+  expect(comp.attack(garyBoard)).toEqual({ status: "miss!" });
 });
